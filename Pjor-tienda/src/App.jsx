989821b440c395277c1ae66957ebb4bb3ad18fd6
Agustin Condado <agustin.condado01@gmail.com>
2026-04-17 15:39:27 -0600

    Auto log in feature added when creating user

--- Pjor-tienda/src/App.jsx
+++ Pjor-tienda/src/App.jsx
@@ -1,42 +1,61 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { MenPage } from './pages/MenPage';
 import { LandingPage } from './pages/LandingPage';
-import { MenPageContextProvider } from './context/MenPageContext';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { MediaPage } from './pages/MediaPage';
-import { WomenPage } from './pages/WomenPage'; // Importa el componente WomenPage
-import { VisionPage } from './pages/VisionPage'
+import { WomenPage } from './pages/WomenPage';
+import { VisionPage } from './pages/VisionPage';
 import { CrewPage } from './pages/CrewPage';
 import CheckoutPage from './pages/CheckoutPage';
+
+import { MenPageContextProvider } from './context/MenPageContext';
+
+// 🔥 AUTH IMPORT
+import { AuthProvider } from './context/authContext';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <div className='body'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='hombre' element={
-            <MenPageContextProvider>
-              <MenPage />
-            </MenPageContextProvider>
-          } />
-          <Route path='mujer' element={
-            <MenPageContextProvider>
-              <WomenPage />
-            </MenPageContextProvider>
-          } />
-          <Route path='media' element={<MediaPage />} />
-          <Route path='crew' element={<CrewPage />} />
-          <Route path='vision' element={<VisionPage />} />
-          <Route path='checkout' element={
-            <MenPageContextProvider>
-              <CheckoutPage />
-            </MenPageContextProvider>
-          } />
-        </Routes>
-      </BrowserRouter>
+
+      {/* 🔥 AUTH GLOBAL WRAPPER */}
+      <AuthProvider>
+
+        <BrowserRouter>
+          <Routes>
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='hombre' element={
+              <MenPageContextProvider>
+                <MenPage />
+              </MenPageContextProvider>
+            } />
+
+            <Route path='mujer' element={
+              <MenPageContextProvider>
+                <WomenPage />
+              </MenPageContextProvider>
+            } />
+
+            <Route path='media' element={<MediaPage />} />
+            <Route path='crew' element={<CrewPage />} />
+            <Route path='vision' element={<VisionPage />} />
+
+            <Route path='checkout' element={
+              <MenPageContextProvider>
+                <CheckoutPage />
+              </MenPageContextProvider>
+            } />
+
+          </Routes>
+        </BrowserRouter>
+
+      </AuthProvider>
+
     </div>
   );
 }

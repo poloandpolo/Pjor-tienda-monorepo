23933ce1bebeeebca9e9a05a9eadcd8a302f780c
Agustin Condado <agustin.condado01@gmail.com>
2026-04-17 15:25:39 -0600

    Auto log in feature added when creating user

--- Pjor-tienda/src/services/api.js
+++ Pjor-tienda/src/services/api.js
@@ -3,23 +3,20 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 export const apiFetch = async (endpoint, options = {}) => {
   const token = localStorage.getItem('jwt');
 
-  console.log('🔑 TOKEN EN apiFetch:', token); // 👈 AQUÍ
-
   if (!token) {
-    console.error('❌ TOKEN MISSING');
+    throw new Error('No authenticated user');
   }
 
   const response = await fetch(`${BASE_URL}${endpoint}`, {
+    ...options,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`, // 🔥 SIEMPRE ENVÍA
+      Authorization: `Bearer ${token}`,
     },
-    ...options,
   });
 
   if (!response.ok) {
     const errorData = await response.json().catch(() => ({}));
-    console.error('API ERROR:', errorData);
     throw new Error(errorData.message || 'Error en la petición');
   }
 

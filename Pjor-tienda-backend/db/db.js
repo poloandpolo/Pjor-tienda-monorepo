5325478d7f1577d0c1b4075cad1e45f5bcc3d2be
Agustin Condado <agustin.condado01@gmail.com>
2026-05-06 15:11:28 -0600

    Order section done

--- Pjor-tienda-backend/db/db.js
+++ Pjor-tienda-backend/db/db.js
@@ -5,4 +5,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const db = knex(knexConfig[env]);
 
+console.log('ENV:', process.env.NODE_ENV);
+console.log('DB HOST:', process.env.DB_HOST);
+
 module.exports = db;

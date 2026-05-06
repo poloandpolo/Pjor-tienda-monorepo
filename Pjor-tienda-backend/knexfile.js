ae918d13028d47370ffd36c5cc098d2627d12776
Agustin Condado <agustin.condado01@gmail.com>
2026-05-06 15:26:56 -0600

    Order section done

--- Pjor-tienda-backend/knexfile.js
+++ Pjor-tienda-backend/knexfile.js
@@ -15,16 +15,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
     pool: {
       min: 0,
       max: 5,
-      acquireTimeoutMillis: 30000,
-      createTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
     },
   },
 };
